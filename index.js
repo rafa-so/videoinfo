a31d1404b58d7eb324ac93d69ef6318500bf234d
Rafael Oliveira <rafael.oliveira@culturainglesa.com.br>
2023-04-10 09:33:57 -0300

    add webContents send to index html page

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const ffmpeg = require('fluent-ffmpeg');
 
 const { app, BrowserWindow, ipcMain } = electron;
 
+let mainWindow;
+
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
@@ -16,6 +18,6 @@ app.on('ready', () => {
 
 ipcMain.on('video:submit', (event, path) => {
   ffmpeg.ffprobe(path, (err, metadata) => {
-    console.log('Video length is: ', metadata.format.duration);
+    mainWindow.webContents.send('video:metadata', metadata.format.duration);
   });
 });
