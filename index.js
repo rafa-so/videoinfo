42015da38df62bb6c8749ac303c13a4f64505bdc
Rafael Oliveira <rafael.oliveira@culturainglesa.com.br>
2023-04-10 08:47:36 -0300

    Add ffproble function and fiz require error within html file index

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
 
 const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
-  const mainWindow = new BrowserWindow();
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 });
 
-ipcMain.on('video:submit', () => {});
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    console.log('Video length is: ', metadata.format.duration);
+  });
+});
